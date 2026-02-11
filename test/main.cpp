eab369574e45c9e215fa09d01a94e23aa7f62931
unknown-0x <254237445+unknown-0x@users.noreply.github.com>
2026-02-11 23:11:00 +0700

    Add simple-test project

--- test/main.cpp
+++ test/main.cpp
@@ -1,68 +1,17 @@
 #include <math/math.hpp>
+#include <simpletest/simpletest.hpp>
 
-#include <cstdio>
+TEST_CASE(Math, Add) {
+  CHECK_EQ(math::Add(1, 2), 3);
+  CHECK_EQ(math::Add(4, 1), 5);
+}
 
-int main(int argc, char** argv) {
-  (void)argc;
-  (void)argv;
+TEST_CASE(Math, Subtract) {
+  CHECK_EQ(math::Subtract(1, 2), -1);
+  CHECK_EQ(math::Subtract(4, 1), 3);
+}
 
-#if defined(_WIN64)
-  printf("OS: Windows (64)\n");
-#elif defined(_WIN32)
-  printf("OS: Windows (32)\n");
-#elif defined(__APPLE__) || defined(__MACH__)
-  printf("OS: MacOS\n");
-#elif defined(__linux__)
-  printf("OS: Linux\n");
-#else
-  printf("OS: Unknown\n");
-#endif
-
-#if defined(NDEBUG)
-  printf("Build type: RELEASE/OPTIMIZED\n");
-#else
-  printf("Build type: DEBUG\n");
-#endif
-
-#if defined(_MSC_VER) && !defined(__clang__)
-  printf("Compiler: MSVC (cl)\n");
-#elif defined(__clang__)
-#if defined(_MSC_VER)
-  printf("Compiler: MSVC (clang-cl)\n");
-#else
-  printf("Compiler: Clang\n");
-#endif
-#elif defined(__GNUC__)
-  printf("Compiler: GCC\n");
-#else
-  printf("Compiler: Unknown\n");
-#endif
-
-#if defined(_M_X64) || defined(__x86_64__)
-  printf("Arch: x64 (64-bit)\n");
-#elif defined(_M_IX86) || defined(__i386__)
-  printf("Arch: x86 (32-bit)\n");
-#elif defined(_M_ARM64) || defined(__aarch64__)
-  printf("Arch: ARM64\n");
-#elif defined(_M_ARM) || defined(__arm__)
-  printf("Arch: ARM (32-bit)\n");
-#else
-  printf("Arch: Unknown\n");
-#endif
-
-  if (math::Add(1, 2) != 3) {
-    return -1;
-  }
-
-  if (math::Subtract(4, 2) != 2) {
-    return -1;
-  }
-
-  if (math::Max(5, 1) != 5) {
-    return -1;
-  }
-
-  printf("All tests passed!\n");
-
-  return 0;
+TEST_CASE(Math, Max) {
+  CHECK_EQ(math::Max(1, 2), 2);
+  CHECK_EQ(math::Max(4, 1), 4);
 }
